e1fe1d658e27d00aa735f723a701ca571fdc83b5
凌亦 <lingyi.zcs@alibaba-inc.com>
2019-11-23 00:51:02 +0800

    update

--- src/app/weight-control-helper/weight-control-helper.component.ts
+++ src/app/weight-control-helper/weight-control-helper.component.ts
@@ -4,12 +4,23 @@ import { EventEmitter } from '@angular/core';
 @Component({
   selector: 'weight-control-helper',
   template: `
-    <div style="color: red; font-weight: bolder;">
+    <div>
       Weight Control Helper is Standby!
+      <span style="color: red; font-weight: bolder;">Current Calorie: {{ calorie }}</span>
     </div>
   `
 })
 
 export class WeightControlHelperComponent {
+  @Input() calorie: number;
+  @Output() warning = new EventEmitter<string>();
+  
   // TODO demo2-helper-warning-logic
+  constructor() {
+    setInterval(() => {
+      if (this.calorie >= 1000) {
+        this.warning.emit('Stop eating or die!');
+      }
+    }, 1000);
+  }
 }
